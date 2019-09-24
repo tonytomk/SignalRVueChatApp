f17c720ade501210c670a8053bbad3dddd4dc8ff
Tony Tom K <tony.tomk@thermofisher.com>
2019-09-24 14:44:51 +0530

    first commit

---
+++ SignalRSampleApp/SignalRSampleApp/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRSampleApp
+{
+    public class ChatHub : Hub
+    {
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine("ChatHub hub connected");
+            return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Console.WriteLine("ChatHub hub disconnected");
+            return base.OnDisconnectedAsync(exception);
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine("Message received");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+
+}
